fdadf454af2cbb8ae8048ec5b5552631ae55e2fc
EDR-9 <68863405+EDR-9@users.noreply.github.com>
2025-01-16 23:15:55 -0600

    Add files via upload

---
+++ manifest.h
@@ -0,0 +1,413 @@
+#ifndef  MANIFEST_H
+#define  MANIFEST_H
+
+
+#define  PROTOCOL_1_ID        0x23
+#define  PROTOCOL_1_EEPROM    0x00
+#define  PROTOCOL_1_ADDR      0x10001000
+
+#define  PROTOCOL_2_ID        0x24
+#define  PROTOCOL_2_EEPROM    0x00
+#define  PROTOCOL_2_ADDR      0x10001800
+
+#define  PROTOCOL_3_ID        0x25
+#define  PROTOCOL_3_EEPROM    0x00
+#define  PROTOCOL_3_ADDR      0x10002000
+
+#define  PROTOCOL_4_ID        0x26
+#define  PROTOCOL_4_EEPROM    0x00
+#define  PROTOCOL_4_ADDR      0x10002800
+
+#define  PROTOCOL_5_ID        0x27
+#define  PROTOCOL_5_EEPROM    0x00
+#define  PROTOCOL_5_ADDR      0x10003000
+
+#define  PROTOCOL_6_ID        0x28
+#if  SE_CUMPLE_ALGUNA_CONDICION_PARA_SABER_SI_EL_PROTOCOLO_ESTA_EN_MEMORIA
+  #define  PROTOCOL_6_EEPROM    0x01
+#else
+  #define  PROTOCOL_6_EEPROM    0x00
+#endif
+#define  PROTOCOL_6_ADDR      0x10003800
+
+#define  PROTOCOL_7_ID        0x29
+#define  PROTOCOL_7_EEPROM    0x00
+#define  PROTOCOL_7_ADDR      0x10004000
+
+#define  PROTOCOL_8_ID        0x2A
+#define  PROTOCOL_8_EEPROM    0x00
+#define  PROTOCOL_8_ADDR      0x10004800
+
+#define  PROTOCOL_9_ID        0x2B
+#define  PROTOCOL_9_EEPROM    0x00
+#define  PROTOCOL_9_ADDR      0x10005000
+
+#define  PROTOCOL_10_ID        0x2C
+#define  PROTOCOL_10_EEPROM    0x00
+#define  PROTOCOL_10_ADDR      0x10005800
+
+#define  PROTOCOL_11_ID        0x2D
+#define  PROTOCOL_11_EEPROM    0x00
+#define  PROTOCOL_11_ADDR      0x10006000
+
+#define  PROTOCOL_12_ID        0x2E
+#define  PROTOCOL_12_EEPROM    0x00
+#define  PROTOCOL_12_ADDR      0x10006800
+
+#define  PROTOCOL_13_ID        0x2F
+#define  PROTOCOL_13_EEPROM    0x00
+#define  PROTOCOL_13_ADDR      0x10007000
+
+#define  PROTOCOL_14_ID        0x30
+#define  PROTOCOL_14_EEPROM    0x00
+#define  PROTOCOL_14_ADDR      0x10007800
+
+#define  PROTOCOL_15_ID        0x31
+#define  PROTOCOL_15_EEPROM    0x00
+#define  PROTOCOL_15_ADDR      0x10008000
+
+#define  PROTOCOL_16_ID        0x32
+#define  PROTOCOL_16_EEPROM    0x00
+#define  PROTOCOL_16_ADDR      0x10008800
+
+#define  PROTOCOL_17_ID        0x33
+#define  PROTOCOL_17_EEPROM    0x00
+#define  PROTOCOL_17_ADDR      0x10009000
+
+#define  PROTOCOL_18_ID        0x34
+#define  PROTOCOL_18_EEPROM    0x00
+#define  PROTOCOL_18_ADDR      0x10009800
+
+#define  PROTOCOL_19_ID        0x35
+#define  PROTOCOL_19_EEPROM    0x00
+#define  PROTOCOL_19_ADDR      0x1000A000
+
+#define  PROTOCOL_20_ID        0x36
+#define  PROTOCOL_20_EEPROM    0x00
+#define  PROTOCOL_20_ADDR      0x1000A800
+
+#define  PROTOCOL_21_ID        0x37
+#define  PROTOCOL_21_EEPROM    0x00
+#define  PROTOCOL_21_ADDR      0x1000B000
+
+#define  PROTOCOL_22_ID        0x38
+#define  PROTOCOL_22_EEPROM    0x00
+#define  PROTOCOL_22_ADDR      0x1000B800
+
+#define  PROTOCOL_23_ID        0x39
+#define  PROTOCOL_23_EEPROM    0x00
+#define  PROTOCOL_23_ADDR      0x1000C000
+
+#define  PROTOCOL_24_ID        0x3A
+#define  PROTOCOL_24_EEPROM    0x00
+#define  PROTOCOL_24_ADDR      0x1000C800
+
+#define  PROTOCOL_25_ID        0x3B
+#define  PROTOCOL_25_EEPROM    0x00
+#define  PROTOCOL_25_ADDR      0x1000D000
+
+#define  PROTOCOL_26_ID        0x3C
+#define  PROTOCOL_26_EEPROM    0x00
+#define  PROTOCOL_26_ADDR      0x1000D800
+
+#define  PROTOCOL_27_ID        0x3D
+#define  PROTOCOL_27_EEPROM    0x00
+#define  PROTOCOL_27_ADDR      0x1000E000
+
+#define  PROTOCOL_28_ID        0x3E
+#define  PROTOCOL_28_EEPROM    0x00
+#define  PROTOCOL_28_ADDR      0x1000E800
+
+#define  PROTOCOL_29_ID        0x3F
+#define  PROTOCOL_29_EEPROM    0x00
+#define  PROTOCOL_29_ADDR      0x1000F000
+
+#define  PROTOCOL_30_ID        0x40
+#define  PROTOCOL_30_EEPROM    0x00
+#define  PROTOCOL_30_ADDR      0x1000F800
+
+#define  PROTOCOL_31_ID        0x41
+#define  PROTOCOL_31_EEPROM    0x00
+#define  PROTOCOL_31_ADDR      0x10010000
+
+#define  PROTOCOL_32_ID        0x42
+#define  PROTOCOL_32_EEPROM    0x00
+#define  PROTOCOL_32_ADDR      0x10010800
+
+#define  PROTOCOL_33_ID        0x43
+#define  PROTOCOL_33_EEPROM    0x00
+#define  PROTOCOL_33_ADDR      0x10011000
+
+#define  PROTOCOL_34_ID        0x44
+#define  PROTOCOL_34_EEPROM    0x00
+#define  PROTOCOL_34_ADDR      0x10011800
+
+#define  PROTOCOL_35_ID        0x45
+#define  PROTOCOL_35_EEPROM    0x00
+#define  PROTOCOL_35_ADDR      0x10012000
+
+#define  PROTOCOL_36_ID        0x46
+#define  PROTOCOL_36_EEPROM    0x00
+#define  PROTOCOL_36_ADDR      0x10012800
+
+#define  PROTOCOL_37_ID        0x47
+#define  PROTOCOL_37_EEPROM    0x00
+#define  PROTOCOL_37_ADDR      0x10013000
+
+#define  PROTOCOL_38_ID        0x48
+#define  PROTOCOL_38_EEPROM    0x00
+#define  PROTOCOL_38_ADDR      0x10013800
+
+#define  PROTOCOL_39_ID        0x49
+#define  PROTOCOL_39_EEPROM    0x00
+#define  PROTOCOL_39_ADDR      0x10014000
+
+#define  PROTOCOL_40_ID        0x4A
+#define  PROTOCOL_40_EEPROM    0x00
+#define  PROTOCOL_40_ADDR      0x10014800
+
+#define  PROTOCOL_41_ID        0x4B
+#define  PROTOCOL_41_EEPROM    0x00
+#define  PROTOCOL_41_ADDR      0x10015000
+
+#define  PROTOCOL_42_ID        0x4C
+#define  PROTOCOL_42_EEPROM    0x00
+#define  PROTOCOL_42_ADDR      0x10015800
+
+#define  PROTOCOL_43_ID        0x4D
+#define  PROTOCOL_43_EEPROM    0x00
+#define  PROTOCOL_43_ADDR      0x10016000
+
+#define  PROTOCOL_44_ID        0x4E
+#define  PROTOCOL_44_EEPROM    0x00
+#define  PROTOCOL_44_ADDR      0x10016800
+
+#define  PROTOCOL_45_ID        0x4F
+#define  PROTOCOL_45_EEPROM    0x00
+#define  PROTOCOL_45_ADDR      0x10017000
+
+#define  PROTOCOL_46_ID        0x50
+#define  PROTOCOL_46_EEPROM    0x00
+#define  PROTOCOL_46_ADDR      0x10017800
+
+#define  PROTOCOL_47_ID        0x51
+#define  PROTOCOL_47_EEPROM    0x00
+#define  PROTOCOL_47_ADDR      0x10018000
+
+#define  PROTOCOL_48_ID        0x52
+#define  PROTOCOL_48_EEPROM    0x00
+#define  PROTOCOL_48_ADDR      0x10018800
+
+#define  PROTOCOL_49_ID        0x53
+#define  PROTOCOL_49_EEPROM    0x00
+#define  PROTOCOL_49_ADDR      0x10019000
+
+#define  PROTOCOL_50_ID        0x54
+#define  PROTOCOL_50_EEPROM    0x00
+#define  PROTOCOL_50_ADDR      0x10019800
+
+#define  PROTOCOL_51_ID        0x55
+#define  PROTOCOL_51_EEPROM    0x00
+#define  PROTOCOL_51_ADDR      0x1001A000
+
+#define  PROTOCOL_52_ID        0x56
+#define  PROTOCOL_52_EEPROM    0x00
+#define  PROTOCOL_52_ADDR      0x1001A800
+
+#define  PROTOCOL_53_ID        0x57
+#define  PROTOCOL_53_EEPROM    0x00
+#define  PROTOCOL_53_ADDR      0x1001B000
+
+#define  PROTOCOL_54_ID        0x58
+#define  PROTOCOL_54_EEPROM    0x00
+#define  PROTOCOL_54_ADDR      0x1001B800
+
+#define  PROTOCOL_55_ID        0x59
+#define  PROTOCOL_55_EEPROM    0x00
+#define  PROTOCOL_55_ADDR      0x1001C000
+
+#define  PROTOCOL_56_ID        0x5A
+#define  PROTOCOL_56_EEPROM    0x00
+#define  PROTOCOL_56_ADDR      0x1001C800
+
+#define  PROTOCOL_57_ID        0x5B
+#define  PROTOCOL_57_EEPROM    0x00
+#define  PROTOCOL_57_ADDR      0x1001D000
+
+#define  PROTOCOL_58_ID        0x5C
+#define  PROTOCOL_58_EEPROM    0x00
+#define  PROTOCOL_58_ADDR      0x1001D800
+
+#define  PROTOCOL_59_ID        0x5D
+#define  PROTOCOL_59_EEPROM    0x00
+#define  PROTOCOL_59_ADDR      0x1001E000
+
+#define  PROTOCOL_60_ID        0x5E
+#define  PROTOCOL_60_EEPROM    0x00
+#define  PROTOCOL_60_ADDR      0x1001E800
+
+#define  PROTOCOL_61_ID        0x5F
+#define  PROTOCOL_61_EEPROM    0x00
+#define  PROTOCOL_61_ADDR      0x1001F000
+
+#define  PROTOCOL_62_ID        0x60
+#define  PROTOCOL_62_EEPROM    0x00
+#define  PROTOCOL_62_ADDR      0x1001F800
+
+#define  PROTOCOL_63_ID        0x61
+#define  PROTOCOL_63_EEPROM    0x00
+#define  PROTOCOL_63_ADDR      0x10020000
+
+#define  PROTOCOL_64_ID        0x62
+#define  PROTOCOL_64_EEPROM    0x00
+#define  PROTOCOL_64_ADDR      0x10020800
+
+#define  PROTOCOL_65_ID        0x63
+#define  PROTOCOL_65_EEPROM    0x00
+#define  PROTOCOL_65_ADDR      0x10021000
+
+#define  PROTOCOL_66_ID        0x64
+#define  PROTOCOL_66_EEPROM    0x00
+#define  PROTOCOL_66_ADDR      0x10021800
+
+#define  PROTOCOL_67_ID        0x65
+#define  PROTOCOL_67_EEPROM    0x00
+#define  PROTOCOL_67_ADDR      0x10022000
+
+#define  PROTOCOL_68_ID        0x66
+#define  PROTOCOL_68_EEPROM    0x00
+#define  PROTOCOL_68_ADDR      0x10022800
+
+#define  PROTOCOL_69_ID        0x67
+#define  PROTOCOL_69_EEPROM    0x00
+#define  PROTOCOL_69_ADDR      0x10023000
+
+#define  PROTOCOL_70_ID        0x68
+#define  PROTOCOL_70_EEPROM    0x00
+#define  PROTOCOL_70_ADDR      0x10023800
+
+#define  PROTOCOL_71_ID        0x69
+#define  PROTOCOL_71_EEPROM    0x00
+#define  PROTOCOL_71_ADDR      0x10024000
+
+#define  PROTOCOL_72_ID        0x6A
+#define  PROTOCOL_72_EEPROM    0x00
+#define  PROTOCOL_72_ADDR      0x10024800
+
+#define  PROTOCOL_73_ID        0x6B
+#define  PROTOCOL_73_EEPROM    0x00
+#define  PROTOCOL_73_ADDR      0x10025000
+
+#define  PROTOCOL_74_ID        0x6C
+#define  PROTOCOL_74_EEPROM    0x00
+#define  PROTOCOL_74_ADDR      0x10025800
+
+#define  PROTOCOL_75_ID        0x6D
+#define  PROTOCOL_75_EEPROM    0x00
+#define  PROTOCOL_75_ADDR      0x10026000
+
+#define  PROTOCOL_76_ID        0x6E
+#define  PROTOCOL_76_EEPROM    0x00
+#define  PROTOCOL_76_ADDR      0x10026800
+
+#define  PROTOCOL_77_ID        0x6F
+#define  PROTOCOL_77_EEPROM    0x00
+#define  PROTOCOL_77_ADDR      0x10027000
+
+#define  PROTOCOL_78_ID        0x70
+#define  PROTOCOL_78_EEPROM    0x00
+#define  PROTOCOL_78_ADDR      0x10027800
+
+#define  PROTOCOL_79_ID        0x71
+#define  PROTOCOL_79_EEPROM    0x00
+#define  PROTOCOL_79_ADDR      0x10028000
+
+#define  PROTOCOL_80_ID        0x72
+#define  PROTOCOL_80_EEPROM    0x00
+#define  PROTOCOL_80_ADDR      0x10028800
+
+#define  PROTOCOL_81_ID        0x73
+#define  PROTOCOL_81_EEPROM    0x00
+#define  PROTOCOL_81_ADDR      0x10029000
+
+#define  PROTOCOL_82_ID        0x74
+#define  PROTOCOL_82_EEPROM    0x00
+#define  PROTOCOL_82_ADDR      0x10029800
+
+#define  PROTOCOL_83_ID        0x75
+#define  PROTOCOL_83_EEPROM    0x00
+#define  PROTOCOL_83_ADDR      0x1002A000
+
+#define  PROTOCOL_84_ID        0x76
+#define  PROTOCOL_84_EEPROM    0x00
+#define  PROTOCOL_84_ADDR      0x1002A800
+
+#define  PROTOCOL_85_ID        0x77
+#define  PROTOCOL_85_EEPROM    0x00
+#define  PROTOCOL_85_ADDR      0x1002B000
+
+#define  PROTOCOL_86_ID        0x78
+#define  PROTOCOL_86_EEPROM    0x00
+#define  PROTOCOL_86_ADDR      0x1002B800
+
+#define  PROTOCOL_87_ID        0x79
+#define  PROTOCOL_87_EEPROM    0x00
+#define  PROTOCOL_87_ADDR      0x1002C000
+
+#define  PROTOCOL_88_ID        0x7A
+#define  PROTOCOL_88_EEPROM    0x00
+#define  PROTOCOL_88_ADDR      0x1002C800
+
+#define  PROTOCOL_89_ID        0x7B
+#define  PROTOCOL_89_EEPROM    0x00
+#define  PROTOCOL_89_ADDR      0x1002D000
+
+#define  PROTOCOL_90_ID        0x7C
+#define  PROTOCOL_90_EEPROM    0x00
+#define  PROTOCOL_90_ADDR      0x1002D800
+
+#define  PROTOCOL_91_ID        0x7D
+#define  PROTOCOL_91_EEPROM    0x00
+#define  PROTOCOL_91_ADDR      0x1002E000
+
+#define  PROTOCOL_92_ID        0x7E
+#define  PROTOCOL_92_EEPROM    0x00
+#define  PROTOCOL_92_ADDR      0x1002E800
+
+#define  PROTOCOL_93_ID        0x7F
+#define  PROTOCOL_93_EEPROM    0x00
+#define  PROTOCOL_93_ADDR      0x1002F000
+
+#define  PROTOCOL_94_ID        0x80
+#define  PROTOCOL_94_EEPROM    0x00
+#define  PROTOCOL_94_ADDR      0x1002F800
+
+#define  PROTOCOL_95_ID        0x81
+#define  PROTOCOL_95_EEPROM    0x00
+#define  PROTOCOL_95_ADDR      0x10030000
+
+#define  PROTOCOL_96_ID        0x82
+#define  PROTOCOL_96_EEPROM    0x00
+#define  PROTOCOL_96_ADDR      0x10030800
+
+#define  PROTOCOL_97_ID        0x83
+#define  PROTOCOL_97_EEPROM    0x00
+#define  PROTOCOL_97_ADDR      0x10031000
+
+#define  PROTOCOL_98_ID        0x84
+#define  PROTOCOL_98_EEPROM    0x00
+#define  PROTOCOL_98_ADDR      0x10031800
+
+#define  PROTOCOL_99_ID        0x85
+#define  PROTOCOL_99_EEPROM    0x00
+#define  PROTOCOL_99_ADDR      0x10032000
+
+#define  PROTOCOL_100_ID        0x86
+#define  PROTOCOL_100_EEPROM    0x00
+#define  PROTOCOL_100_ADDR      0x10032800
+
+
+#endif  /* MANIFEST_H */
+
+
+
